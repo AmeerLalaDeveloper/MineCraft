de4c9ab6f419facc896c950413c98b194e66c296
AmeerLalaDeveloper <laklaw100@gmail.com>
2021-10-05 10:47:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,212 @@
+const forms=[
+    [
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','c','c','c','s','s','c','c','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','c','c','c','s','s','c','s','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','c','c','c','s','s','s','s','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','w','s','s','s','s','s','s','s',
+        's','s','s','s','o','s','s','s','s','s','s','s','w','s','s','s','s','s','s','s',
+        's','s','s','o','o','o','s','s','s','s','s','s','w','s','s','s','s','s','s','s',
+        'l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+    ],
+    [
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','c','c','s','c','c','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','c','c','s','c','c','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','c','c','c','s','s','c','c','c','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','c','s','s','s','s','c','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','c','c','c','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','t','t','t','t','t','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','t','t','t','t','t','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','t','t','t','t','t','s','s','s','s','s','s','s','s','o','o','s','s','s',
+        's','s','t','t','t','t','t','s','s','s','s','s','s','s','s','o','o','s','s','s',
+        's','s','s','s','w','s','s','s','s','s','s','s','s','s','s','o','o','s','s','s',
+        's','s','s','s','w','s','s','s','s','s','s','s','s','s','s','o','o','s','s','s',
+        's','s','s','s','w','s','s','s','s','s','s','s','s','s','s','o','o','s','s','s',
+        'l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+    ],
+    [
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','c','c','c','c','s','s','c','c','c','c','s','s','c','c','c','c','s','s','s',
+        's','c','c','c','c','s','s','c','c','c','c','s','s','c','c','c','c','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','t','t','t','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','t','t','t','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','t','t','t','s','s','s','s','s','s','s','s','s','t','t','t','s','s',
+        's','s','s','t','t','t','s','s','s','t','t','t','s','s','s','t','t','t','s','s',
+        's','s','s','s','w','s','s','s','s','t','t','t','s','s','s','t','t','t','s','s',
+        's','s','s','s','w','s','s','s','s','s','w','s','s','s','s','s','w','s','s','s',
+        's','s','s','s','w','o','o','o','o','o','w','o','o','o','o','o','w','s','s','s',
+        'l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+    ],[
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','c','c','c','s','s','c','c','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','c','c','c','s','s','c','s','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','c','c','c','s','s','s','s','s','s','s','s','s','s','c','c','c','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','s','s','s','t','t','t','t','t','s','s','s','s','s',
+        's','s','s','s','s','s','s','t','s','s','t','t','t','t','t','t','t','s','s','s',
+        's','s','s','s','s','s','s','t','s','s','t','t','t','t','t','t','t','s','s','s',
+        's','s','s','s','s','s','t','t','t','s','s','s','w','s','s','w','s','s','s','s',
+        's','s','o','o','o','s','s','w','s','o','s','s','w','s','s','w','s','w','s','s',
+        's','s','o','o','o','s','s','w','s','o','s','s','w','s','s','w','s','w','s','s',
+        'l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l','l',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+        'd','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d','d',
+    ]
+    ]
+ const randForm=forms[Math.floor(Math.random()*4)]
+ const gameBoard=document.querySelector('.game-board')
+ let currentItem=null;
+ let currentTool=null;
+ let currentBox=null;
+
+function draw(){
+    for(let i=0; i<randForm.length; i++) {
+        const div=document.createElement('div')
+        div.classList.add('box')
+    
+        if(randForm[i]=='s')
+            div.classList.add('sky')
+        else if(randForm[i]=='c')
+           div.classList.add('cloud')
+        else if(randForm[i]=='w')
+          div.classList.add('wood')
+        else if(randForm[i]=='t')
+            div.classList.add('tree');
+        else if(randForm[i]=='o')
+            div.classList.add('stone');
+        else if(randForm[i]=='e')
+            div.classList.add('water');
+        else if(randForm[i]=='l')
+            div.classList.add('land');
+        else if(randForm[i]=='d')
+            div.classList.add('dirt');
+        gameBoard.append(div)
+    }
+    }
+	draw() ;
+function updateCounter(box){
+        let itemToUpdate=null;
+        items.forEach(item=>{  
+          const itemClass=item.className.split(' ')[1];
+            if(itemClass==currentBox)
+                itemToUpdate=item;
+        })
+        if(itemToUpdate==null)
+        return;
+        let currentAmount=parseInt(itemToUpdate.firstChild.textContent);
+        currentAmount++;
+        itemToUpdate.firstChild.textContent=currentAmount.toString();
+}
+function removeBox(box){
+
+  const toolName=currentTool.className.split(' ')[1];  
+ if((currentBox=='wood'&&toolName=='axe')
+ ||(currentBox=='tree'&&toolName=='axe')
+ ||((currentBox=='dirt'&&toolName=='shovel')
+ ||(currentBox=='land'&&toolName=='shovel'))
+ ||((currentBox=='stone'&&toolName=='pickaxe'))){
+    box.classList.remove(currentBox)
+    box.classList.add('sky')
+    updateCounter(box);
+ }
+}
+function addBox(box){
+    
+    //TODO Im Here
+    let count =parseInt(currentItem.firstChild.textContent)
+    if(count==0)return;
+    let idx=boxes.indexOf(box);
+    let length=20;
+    const itemName=currentItem.className.split(' ')[1];
+    const boxName=box.className.split(' ')[1];
+    if(boxName=='sky'&&boxes[idx+length].className.split(' ')[1]!='sky'){
+    box.classList.remove(boxName);
+    box.classList.add(itemName)
+    count--;
+    currentItem.firstChild.textContent=count.toString();
+    }
+   
+
+    
+    
+        
+   
+  
+
+}
+
+const tools=Array.from(document.querySelectorAll('.tool'))
+const back=document.querySelector('.back')
+const items=Array.from(document.querySelectorAll('.item'))
+const reset=document.querySelector('.reset')
+ const boxes=Array.from(document.querySelectorAll('.box'))
+boxes.forEach(box=>{
+    box.addEventListener('click',function(e){
+        currentBox=this.className.split(' ')[1];
+        if(currentTool==null&&currentItem!=null)    
+        addBox(this)
+        if(currentItem==null&&currentTool!=null)
+        removeBox(this);
+    })
+    currentBox=null;
+})
+items.forEach(item=>{
+
+    item.addEventListener('click',function(e){
+        currentTool=null;
+        if(this.firstChild.textContent=='0')
+        return;
+        else
+        currentItem=this;
+    })
+    currentItem=null;
+})
+tools.forEach(tool=>{
+    tool.addEventListener('click',function(e){
+        currentItem=null;
+        currentTool=this;
+    })
+  currentTool=null;
+})
+reset.addEventListener('click',function(e) {
+location.reload();
+})
+
+
+back.addEventListener('click',function(e) {
+
+})
